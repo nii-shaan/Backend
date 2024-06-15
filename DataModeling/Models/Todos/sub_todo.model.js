6a7d0980ab463e6916449e604d427ddabeddb8e2
Nishan Bista <nishanbista525@gmail.com>
2024-06-15 18:39:04 +0545

    todo data modeling

---
+++ DataModeling/Models/Todos/sub_todo.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const sub_todoSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    complete: {
+      type: Boolean,
+      default: false,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const sub_todo = mongoose.model("Sub_todo", sub_todoSchema);
---
+++ DataModeling/Models/Todos/todo.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const todoSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    complete: {
+      type: boolean,
+      default: false,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+
+    subTodos: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Sub_todo",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export const Todo = mongoose.model("Todo", todoSchema);
